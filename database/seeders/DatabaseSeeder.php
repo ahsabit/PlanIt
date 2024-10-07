9369b076165eb41721c184ad5d52154fe9dfb604
ahsabit <ahsabit6@gmail.com>
2024-10-07 23:23:09 +0600

    second commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,10 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('123'),
+            'email_verified_at' => now(),
         ]);
+
+        Project::factory()->count(40)->hasTasks(20)->create();
     }
 }
